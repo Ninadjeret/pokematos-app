c06dd23fa1d0dbd166ee34692df8ac65bf213f4e
Ninadjeret <myflowersarebeautiful@gmail.com>
2019-02-12 07:23:22 +0100

    API de création de Raid

--- app/Http/Controllers/RaidController.php
+++ app/Http/Controllers/RaidController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\City;
 use App\Models\Raid;
+use App\Models\Stop;
 
 class RaidController extends Controller {
 
@@ -13,6 +14,31 @@ class RaidController extends Controller {
             ->where('start_time', '>', date('Y-m-d H:i:s') )
             ->get();
         return response()->json($raids, 200);
+    }
+
+    public function create( City $city, Request $request ) {
+        $gym = Stop::find($request->params['gym_id']);
+
+        if( $gym->getActiveRaid() ) {
+
+        }
+
+        elseif( $gym->getFutureRaid() ) {
+
+        }
+
+        else {
+            $raid = new Raid();
+            $raid->city_id = $city->id;
+            $raid->gym_id = $request->params['gym_id'];
+            $raid->egg_level = $request->params['egg_level'];
+            $raid->pokemon_id = $request->params['pokemon_id'];
+            $raid->start_time = $request->params['start_time'];
+            $raid->save();
+            return response()->json($raid, 200);
+        }
+
+        return response()->json($gym->getFutureRaid(), 200);
     }
 
 }
