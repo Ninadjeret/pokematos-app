b38ffdbf07d08edfd4107d1d88ba848b129da762
Ninadjeret <myflowersarebeautiful@gmail.com>
2019-02-24 21:01:58 +0100

    authentification des routes api

--- app/Http/Controllers/GymController.php
+++ app/Http/Controllers/GymController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\City;
 use App\Models\Stop;
+use Illuminate\Support\Facades\Log;
 
 class GymController extends Controller {
 
     public function getCityGyms(City $city, Request $request){
-        $gyms = Stop::where('city_id', $city->id)
+        $gyms = Stop::where('city_id', '=', $city->id)
             ->where('gym', 1)
             ->get();
         return response()->json($gyms, 200);
