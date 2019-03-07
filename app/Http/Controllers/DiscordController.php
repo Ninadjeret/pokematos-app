3fb38e830c359f811f0e078ce09d8d798db2de11
Ninadjeret <myflowersarebeautiful@gmail.com>
2019-03-07 18:49:59 +0100

    Prise en compte des statuts admins lors de l'accès à la map

--- app/Http/Controllers/DiscordController.php
+++ app/Http/Controllers/DiscordController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
-use GuzzleHttp\Client;
-use RestCord\DiscordClient;
 use App\User;
 use App\Models\City;
 use App\Models\Guild;
+use GuzzleHttp\Client;
+use RestCord\DiscordClient;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
 
 class DiscordController extends Controller {
 
@@ -65,32 +66,42 @@ class DiscordController extends Controller {
             foreach( $user_guilds as $user_guild ) {
                 $guild = Guild::where( 'discord_id', $user_guild->id )->first();
                 if( $guild ) {
-                    //var_dump( $discord->guild->getGuild(['guild.id' => (int) $guild->discord_id]));
+
                     try {
-                        /*$result = $discord->guild->getGuildMember(array(
+                        $result = $discord->guild->getGuildMember(array(
                             'guild.id' => (int) $guild->discord_id,
                             'user.id' => (int) $user->discord_id,
-                        ));*/
+                        ));
 
-                        //Gestion des droits d'accès
-                        /*if ($result && $guild->access_rule == 'everyone') {
-                            $auth = true;
-                        } elseif ($result && $guild->access_rule == 'specific_roles' && !empty(array_intersect($guild->authorized_roles, $result->roles))) {
-                            $auth = true;
+                        if( $result ) {
+
+                            //Gestion des droits d'accès
+                            if( empty($guild->settings->map_access_rule) || $guild->settings->map_access_rule == 'everyone' ) {
+                                $auth = true;
+                            } elseif( $guild->settings->map_access_rule == 'specific_roles' && !empty(array_intersect($guild->settings->map_access_roles, $result->roles))) {
+                                $auth = true;
+                            }
+
+                            //Gestion des prvilèges d'admin
+                            $admin = false;
+                            if ( !empty($guild->settings->map_access_admin_roles) && !empty(array_intersect($guild->settings->map_access_admin_roles, $result->roles))) {
+                                $admin = true;
+                            }
+
+                            $guilds[] = [
+                                'id' => $guild->id,
+                                'admin' => $admin,
+                            ];
+
                         }
 
                         //Gestion des prvilèges d'admin
-                        if (!empty($guild->getMapAdminRoles()) && $result && !empty(array_intersect($guild->getMapAdminRoles(), $result->roles))) {
-                            $admin[] = $community->wpId;
-                        }*/
+                        /***/
                     } catch (Exception $e) {
                         error_log('Exception reçue : ' . $e->getMessage());
                     }
-                    $auth = true;
-                    $guilds[] = [
-                        'id' => $guild->id,
-                        'admin' => true,
-                    ];
+
+
                 }
 
             }
@@ -147,10 +158,15 @@ class DiscordController extends Controller {
         $discord = new DiscordClient(['token' => config('discord.token')]);
         $roles = $discord->guild->getGuildRoles(['guild.id' => $guild->discord_id]);
         $return = array();
+        $order = array();
         foreach ($roles as $key => $row) {
-            $return[$key] = $row->name;
+            $return[] = [
+                'name' => $row->name,
+                'id' => (string) $row->id
+            ];
+            $order[$key] = $row->name;
         }
-        array_multisort($return, SORT_ASC|SORT_NATURAL|SORT_FLAG_CASE, $roles);
-        return response()->json($roles, 200);
+        array_multisort($order, SORT_ASC|SORT_NATURAL|SORT_FLAG_CASE, $return);
+        return response()->json($return, 200);
     }
 }
