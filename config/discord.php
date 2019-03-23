d6a1c1dbef4ed26a17ef0c3fd3d325869acb062e
Ninadjeret <myflowersarebeautiful@gmail.com>
2019-03-23 09:54:57 +0100

    Endpoints des roles pour le bot

---
+++ config/discord.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'id' => '553675934570643467',
+    'secret' => 'YK0_QEXczCc5jLSNZQuNDRUqcyPrUpzd',
+    'token' => 'NTUzNjc1OTM0NTcwNjQzNDY3.D2Rywg.6lLAOszGGRoeZYQlpzWQq54GvDE',
+    'callback'  => 'http://127.0.0.1:8000/login/discord/callback'
+];
