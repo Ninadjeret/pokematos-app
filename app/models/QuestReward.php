de026d641288b2d2f60d904b57d6cca5c11c015e
Ninadjeret <myflowersarebeautiful@gmail.com>
2019-07-28 21:54:18 +0200

    add reward images on admin quests screen

--- app/models/QuestReward.php
+++ app/models/QuestReward.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class QuestReward extends Model
 {
     protected $table = 'quest_rewards';
+    protected $appends = ['thumbnail_url'];
+
+    public function getThumbnailUrlAttribute() {
+        return 'https://assets.profchen.fr/img/rewards/reward_'.$this->type.'.png';
+    }
 }
