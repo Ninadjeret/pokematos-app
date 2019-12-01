7fef5ec50060342f92e4a2c9608a7a619abd1335
Ninadjeret <myflowersarebeautiful@gmail.com>
2019-12-01 21:45:31 +0100

    front routes

---
+++ resources/js/components/admin/Logs.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="settings-section">
+            <v-list>
+            <template v-for="(log, index) in logs">
+              <div class="log" :key="log.id">
+
+                  <div class="log__content" v-if="log.type == 'raid'">
+                      <p>{{log.created_at}}</p>
+                  </div>
+
+              </div>
+              <v-divider></v-divider>
+            </template>
+          </v-list>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AdminLogs',
+        data() {
+            return {
+                logs: [],
+            }
+        },
+        created() {
+            this.fetchLogs();
+        },
+        methods: {
+            fetchLogs() {
+                axios.get('/api/user/guilds/'+this.$route.params.id+'/logs').then( res => {
+                    console.log('tutu')
+                    this.logs = res.data;
+                });
+            },
+        }
+    }
+</script>
