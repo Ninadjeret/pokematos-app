ac452d083ffa6bf632a883a5f92a53cfccafe1d3
Ninadjeret <myflowersarebeautiful@gmail.com>
2020-04-07 20:40:35 +0200

    db migration

---
+++ database/migrations/2020_04_07_203204_create_events_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->string('type');
+            $table->integer('relation_id');
+            $table->string('channel_discord_id');
+            $table->timestamps();
+        });
+
+        Schema::create('event_trains', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('event_id');
+            $table->string('message_discord_id');
+            $table->timestamps();
+        });
+
+        Schema::create('event_train_steps', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('train_id');
+            $table->string('type')->default('stop');
+            $table->integer('stop_id')->nullable();
+            $table->dateTime('start_time')->nullable();
+            $table->float('duration')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('checked')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
