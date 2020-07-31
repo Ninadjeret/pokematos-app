e74e36034762b3ffbba76f332f4fe2c4ce54733a
Florian Chaillou <myflowersarebeautiful@gmail.com>
2020-07-31 21:51:03 +0200

    nickname support for rocket reports

--- app/Models/RocketConnector.php
+++ app/Models/RocketConnector.php
@@ -39,51 +39,55 @@ class RocketConnector extends Model
 
     public $roles, $emojis, $channels;
 
-    public function getFilteredBossesAttribute() {
-        if( empty($this->filter_boss_bosses) ) {
+    public function getFilteredBossesAttribute()
+    {
+        if (empty($this->filter_boss_bosses)) {
             return [];
         }
         $bosses = [];
-        foreach( $this->filter_boss_bosses as $boss_id ) {
+        foreach ($this->filter_boss_bosses as $boss_id) {
             $boss = RocketBoss::find($boss_id);
-            if( $boss ) {
+            if ($boss) {
                 $bosses[] = $boss;
             }
         }
         return $bosses;
     }
 
-    public function getFilteredZonesAttribute() {
-        if( empty($this->filter_stop_zone) ) {
+    public function getFilteredZonesAttribute()
+    {
+        if (empty($this->filter_stop_zone)) {
             return [];
         }
         $zones = [];
-        foreach( $this->filter_stop_zone as $zone_id ) {
+        foreach ($this->filter_stop_zone as $zone_id) {
             $zone = Zone::find($zone_id);
-            if( $zone ) {
+            if ($zone) {
                 $zones[] = $zone;
             }
         }
         return $zones;
     }
 
-    public function getFilteredStopsAttribute() {
-        if( empty($this->filter_stop_stop) ) {
+    public function getFilteredStopsAttribute()
+    {
+        if (empty($this->filter_stop_stop)) {
             return [];
         }
         $stops = [];
-        foreach( $this->filter_stop_stop as $stop_id ) {
+        foreach ($this->filter_stop_stop as $stop_id) {
             $stop = Stop::find($stop_id);
-            if( $stop ) {
+            if ($stop) {
                 $stops[] = $stop;
             }
         }
         return $stops;
     }
 
-    public function postMessage( $invasion, $announce ) {
-        if( empty( $this->channel_discord_id ) ) return false;
-        $guild = Guild::find( $this->guild_id );
+    public function postMessage($invasion, $announce)
+    {
+        if (empty($this->channel_discord_id)) return false;
+        $guild = Guild::find($this->guild_id);
 
         //On initialise les infos discord
         $discord = new DiscordClient(['token' => config('discord.token')]);
@@ -98,15 +102,15 @@ class RocketConnector extends Model
         ));
 
         //Récupération du message selon le format choisi
-        if( $this->format == 'auto' ) {
+        if ($this->format == 'auto') {
             $content = '';
-            $embed = $this->getEmbedMessage($invasion, $announce);
-        } elseif( $this->format == 'custom' ) {
-            $content = $this->getCustomMessage( $invasion, $announce );
+            $embed = $this->getEmbedMessage($invasion, $announce, $guild);
+        } elseif ($this->format == 'custom') {
+            $content = $this->getCustomMessage($invasion, $announce, $guild);
             $embed = [];
-        } elseif( $this->format == 'both' ) {
-            $content = $this->getCustomMessage( $invasion, $announce );
-            $embed = $this->getEmbedMessage($invasion, $announce);
+        } elseif ($this->format == 'both') {
+            $content = $this->getCustomMessage($invasion, $announce, $guild);
+            $embed = $this->getEmbedMessage($invasion, $announce, $guild);
         }
 
         //On poste le message sur Discord et on log
@@ -126,73 +130,74 @@ class RocketConnector extends Model
         } catch (Exception $e) {
             return false;
         }
-
     }
 
-    public function getCustomMessage( $invasion, $announce ) {
-        return $this->translate( $this->custom_message, $invasion );
+    public function getCustomMessage($invasion, $announce, $guild)
+    {
+        return $this->translate($this->custom_message, $invasion, $guild);
     }
 
-    public function translate( $message, $invasion ) {
-        $username = ( $invasion->getLastUserAction()->getUser() ) ? $invasion->getLastUserAction()->getUser()->name : false;
+    public function translate($message, $invasion, $guild)
+    {
+        $username = ($invasion->getLastUserAction()->getUser()) ? $invasion->getLastUserAction()->getUser()->getNickname($guild->id) : false;
 
         $role_poi_lie = Role::where('gym_id', $invasion->getStop()->id)->first();
-        $role_zone_liee = ( $invasion->getStop()->zone ) ? Role::where('zone_id', $invasion->getStop()->zone->id)->first() : false ;
+        $role_zone_liee = ($invasion->getStop()->zone) ? Role::where('zone_id', $invasion->getStop()->zone->id)->first() : false;
 
         //Gestion des tags
         $patterns = array(
-            'rocketboss_name' => html_entity_decode( $invasion->boss->name ),
-            'rocketboss_pokemon_1' => ( !empty($invasion->pokemon_step1) ) ? $invasion->pokemon_step1->name : '?',
-            'rocketboss_pokemon_2' => ( !empty($invasion->pokemon_step2) ) ? $invasion->pokemon_step2->name : '?',
-            'rocketboss_pokemon_3' => ( !empty($invasion->pokemon_step3) ) ? $invasion->pokemon_step3->name : '?',
+            'rocketboss_name' => html_entity_decode($invasion->boss->name),
+            'rocketboss_pokemon_1' => (!empty($invasion->pokemon_step1)) ? $invasion->pokemon_step1->name : '?',
+            'rocketboss_pokemon_2' => (!empty($invasion->pokemon_step2)) ? $invasion->pokemon_step2->name : '?',
+            'rocketboss_pokemon_3' => (!empty($invasion->pokemon_step3)) ? $invasion->pokemon_step3->name : '?',
 
             'pokestop_nom' => $invasion->getStop()->niantic_name,
             'pokestop_nom_nettoye' => Helpers::sanitize($invasion->getStop()->niantic_name),
             'pokestop_nom_custom' => $invasion->getStop()->name,
             'pokestop_nom_custom_nettoye' => Helpers::sanitize($invasion->getStop()->name),
             'pokestop_description' => $invasion->getStop()->description,
-            'pokestop_zone' => ( !empty(  $invasion->getStop()->zone ) ) ?  $invasion->getStop()->zone->name : false,
-            'pokestop_zone_nettoye' => ( !empty(  $invasion->getStop()->zone ) ) ?  Helpers::sanitize($invasion->getStop()->zone->name) : false,
-            'pokestop_gmaps' => ( !empty(  $invasion->getStop()->google_maps_url ) ) ?  $invasion->getStop()->google_maps_url : false,
+            'pokestop_zone' => (!empty($invasion->getStop()->zone)) ?  $invasion->getStop()->zone->name : false,
+            'pokestop_zone_nettoye' => (!empty($invasion->getStop()->zone)) ?  Helpers::sanitize($invasion->getStop()->zone->name) : false,
+            'pokestop_gmaps' => (!empty($invasion->getStop()->google_maps_url)) ?  $invasion->getStop()->google_maps_url : false,
 
-            'role_poi_lie' => ( !empty($role_poi_lie) ) ? "@{$role_poi_lie->name}" : '',
-            'role_zone_liee' => ( !empty($role_zone_liee) ) ? "@{$role_zone_liee->name}" : '',
+            'role_poi_lie' => (!empty($role_poi_lie)) ? "@{$role_poi_lie->name}" : '',
+            'role_zone_liee' => (!empty($role_zone_liee)) ? "@{$role_zone_liee->name}" : '',
 
             'utilisateur' => $username,
         );
-        foreach( $patterns as $pattern => $valeur ) {
-            $message = str_replace( '{'.$pattern.'}', $valeur, $message );
+        foreach ($patterns as $pattern => $valeur) {
+            $message = str_replace('{' . $pattern . '}', $valeur, $message);
         }
 
         //Gestion des mentions
-        if( strstr( $message, '@' ) ) {
-            foreach( $this->roles as $role ) {
-                if( strstr( $message, '@'.$role->name ) ) {
-                    $message = str_replace('@'.$role->name, '<@&'.$role->id.'>', $message);
+        if (strstr($message, '@')) {
+            foreach ($this->roles as $role) {
+                if (strstr($message, '@' . $role->name)) {
+                    $message = str_replace('@' . $role->name, '<@&' . $role->id . '>', $message);
                 }
             }
         }
 
-        if( $username && strstr( $message, '@'.$username ) ) {
+        if ($username && strstr($message, '@' . $username)) {
             $user = $invasion->getLastUserAction()->getUser();
-            $message = str_replace('@'.$username, '<@!'.$user->discord_id.'>', $message);
+            $message = str_replace('@' . $username, '<@!' . $user->discord_id . '>', $message);
         }
 
         //Gestion des salons #
-        if( strstr( $message, '#' ) ) {
-            foreach( $this->channels as $channel ) {
-                if( strstr( $message, '#'.$channel->name ) ) {
-                    $message = str_replace('#'.$channel->name, '<#'.$channel->id.'>', $message);
+        if (strstr($message, '#')) {
+            foreach ($this->channels as $channel) {
+                if (strstr($message, '#' . $channel->name)) {
+                    $message = str_replace('#' . $channel->name, '<#' . $channel->id . '>', $message);
                 }
             }
         }
 
         //Gestion des emojis
-        if( strstr( $message, ':' ) ) {
-            if( !empty($this->emojis) ) {
-                foreach( $this->emojis as $emoji ) {
-                    if( strstr( $message, ':'.$emoji->name.':' ) ) {
-                        $message = str_replace(':'.$emoji->name.':', '<:'.$emoji->name.':'.$emoji->id.'>', $message);
+        if (strstr($message, ':')) {
+            if (!empty($this->emojis)) {
+                foreach ($this->emojis as $emoji) {
+                    if (strstr($message, ':' . $emoji->name . ':')) {
+                        $message = str_replace(':' . $emoji->name . ':', '<:' . $emoji->name . ':' . $emoji->id . '>', $message);
                     }
                 }
             }
@@ -206,14 +211,15 @@ class RocketConnector extends Model
         return $message;
     }
 
-    public function getEmbedMessage( $invasion, $announce ) {
+    public function getEmbedMessage($invasion, $announce, $guild)
+    {
         $stop = $invasion->getStop();
         $title = "Invasion de {$invasion->boss->name} au Pokéstop {$stop->name}";
         $img_url = $invasion->boss->thumbnail;
 
-        $description = ( !empty($invasion->pokemon_step1) ) ? ":one: {$invasion->pokemon_step1->name}\r\n" : ":one: ?\r\n" ;
-        $description .= ( !empty($invasion->pokemon_step2) ) ? ":two: {$invasion->pokemon_step2->name}\r\n" : ":two: ?\r\n" ;
-        $description .= ( !empty($invasion->pokemon_step3) ) ? ":three: {$invasion->pokemon_step3->name}" : ":three: ?" ;
+        $description = (!empty($invasion->pokemon_step1)) ? ":one: {$invasion->pokemon_step1->name}\r\n" : ":one: ?\r\n";
+        $description .= (!empty($invasion->pokemon_step2)) ? ":two: {$invasion->pokemon_step2->name}\r\n" : ":two: ?\r\n";
+        $description .= (!empty($invasion->pokemon_step3)) ? ":three: {$invasion->pokemon_step3->name}" : ":three: ?";
 
         //On formatte le embed
         $data = array(
@@ -232,5 +238,4 @@ class RocketConnector extends Model
 
         return $data;
     }
-
 }
