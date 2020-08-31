4d3f55a2fe1c2421d61d92ab03a842daa00b6aa0
Florian Chaillou <myflowersarebeautiful@gmail.com>
2020-08-31 08:53:25 +0200

    CP support for mega-raids

--- app/Core/Raids/RaidHelpers.php
+++ app/Core/Raids/RaidHelpers.php
@@ -9,7 +9,7 @@ class RaidHelpers
 {
   public static function getPokemonForCp($raid)
   {
-    if ($raid->egg_level != 6) return $raid->pokemon;
+    if ($raid->egg_level != 7) return $raid->pokemon;
     return Pokemon::where('pokedex_id', $raid->pokemon->pokedex_id)
       ->where('form_id', '00')
       ->first();
